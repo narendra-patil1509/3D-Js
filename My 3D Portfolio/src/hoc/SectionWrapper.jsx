35a25e16d747fbafe4b5edd885eebf9397d2a0b2
narendra-patil1509 <np738587@gmail.com>
2024-08-02 17:20:00 +0530

    Update SectionWrapper.jsx

--- My 3D Portfolio/src/hoc/SectionWrapper.jsx
+++ My 3D Portfolio/src/hoc/SectionWrapper.jsx
@@ -7,12 +7,14 @@ const SectionWrapper = () => {
     function HOC(){
         return (
             <motion.section
-            variants={staggerContainer()}
+            variants={ staggerContainer() }
             initial="hidden"
             whileInView="show"
             viewport={{once: true,amount: 0.25}}
             className={`${styles.padding} max-w-7xl mx-auto relative z-0`}>
-                <span className="hash-span" id={idName}></span>
+                <span className="hash-span" id={idName}>
+                    &nbsp;
+                </span>
                 <Component />
             </motion.section>
     )}
