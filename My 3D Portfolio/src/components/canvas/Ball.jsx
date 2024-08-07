ba84faca2b6d0f0b7e37cf9b6134f8935f792455
narendra-patil1509 <np738587@gmail.com>
2024-08-07 17:11:28 +0530

    Update Ball.jsx

--- My 3D Portfolio/src/components/canvas/Ball.jsx
+++ My 3D Portfolio/src/components/canvas/Ball.jsx
@@ -12,8 +12,20 @@ const Ball = (props) => {
     floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0,0,0.05]}/>
-      <mesh castShadow receiveShadow scale={2.25}>
+      <mesh castShadow receiveShadow scale={2.25}> 
         <icosahedronGeometry args={[1,1]}/>
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal 
+          position={[0,0,1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          flatShading
+          map={decal}
+        />
       </mesh>
     </Float>
   )
