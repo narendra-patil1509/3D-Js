92f693f0ffc32831c62eb98948f0176c17471119
narendra-patil1509 <np738587@gmail.com>
2024-08-15 16:50:09 +0530

    having render issue

--- My 3D Portfolio/src/components/canvas/Computers.jsx
+++ My 3D Portfolio/src/components/canvas/Computers.jsx
@@ -62,7 +62,7 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={ Math.PI / 2 }
           minPolarAngle={ Math.PI / 2 }/>
-          <Computers isMobile={isMobile} />
+          {/* <Computers isMobile={isMobile} /> */}
       </Suspense>
       <Preload all/>
     </Canvas>
