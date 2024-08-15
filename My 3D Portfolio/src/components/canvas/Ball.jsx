4ee048b04f0e5946f3c9049f0ba8b13c6e6fe778
narendra-patil1509 <np738587@gmail.com>
2024-08-15 16:49:18 +0530

    Ball Component bug not solved

--- My 3D Portfolio/src/components/canvas/Ball.jsx
+++ My 3D Portfolio/src/components/canvas/Ball.jsx
@@ -10,8 +10,8 @@ const Ball = (props) => {
     <Float speed={1.75}
     rotationIntensity={1}
     floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0,0,0.05]}/>
+        <ambientLight intensity={0.25} />
+        <directionalLight position={[0,0,0.05]}/>
       <mesh castShadow receiveShadow scale={2.25}> 
         <icosahedronGeometry args={[1,1]}/>
         <meshStandardMaterial
@@ -33,20 +33,14 @@ const Ball = (props) => {
 
 const BallCanvas = ({ icon }) => {
   return(
-    <Canvas 
-      frameLoop="demand"
-      gl={{ preserveDrawingBuffer:true }}
-    >
-      <Suspense 
-        fallback={<CanvasLoader />}
-      >
+    <Canvas frameloop="demand"gl={{ preserveDrawingBuffer:true }}>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false}/>
-          <Ball imgUrl={icon} />
-
+        {/* <Ball imgUrl={icon} /> */}
       </Suspense>
       <Preload all/>
     </Canvas>
   )
 }
 
-export default Ball
+export default BallCanvas;
