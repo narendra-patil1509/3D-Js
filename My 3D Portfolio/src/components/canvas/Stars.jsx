297f4dd0b9e864707b1cf9fd23e1622aecdf7078
narendra-patil1509 <np738587@gmail.com>
2024-08-15 16:50:30 +0530

    having render issue

--- My 3D Portfolio/src/components/canvas/Stars.jsx
+++ My 3D Portfolio/src/components/canvas/Stars.jsx
@@ -33,7 +33,7 @@ const StarsCanvas = () => {
     <div className="w-full h-auto absolute inset-0 z-[-1]">
       <Canvas camera={{position:[0,0,1]}}>
         <Suspense fallback={null}>
-          <Stars />
+          {/* <Stars /> */}
         </Suspense>
         <Preload all/>
       </Canvas>
